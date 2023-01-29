201a9af298658ff9d350f7d1305f61b605d2eb4e
Naveen1st <naveenbanoth4889@gmail.com>
2023-01-29 10:06:05 +0530

    second

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // complete the given function
-// complete the given function
 
 function palindrome(str){
-	let ans=""
-	for(let i=str.length-1;i>=0;i--){
-		ans+=str[i]
-	}
-	if(str==ans){
-		return true
-	}
-	return false
+    let rev_str = "";
+    for( let i = str.length-1 ;i >= 0 ;i--)
+    {
+      rev_str+= str[i];
+    }
+    // return reverse string
+    if (str.toLowerCase()===rev_str.toLowerCase()) {
+        return true
+    } else {
+        return false
+    }
+
 }
 module.exports = palindrome
