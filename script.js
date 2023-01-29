acaa5f77c0d4545913a6823b4f80c6a3ef70766a
Naveen1st <naveenbanoth4889@gmail.com>
2023-01-29 10:03:13 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
+// complete the given function
 
 function palindrome(str){
-
+	let ans=""
+	for(let i=str.length-1;i>=0;i--){
+		ans+=str[i]
+	}
+	if(str==ans){
+		return true
+	}
+	return false
 }
 module.exports = palindrome
